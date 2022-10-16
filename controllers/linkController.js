225489970bbbaa836b0873695ffee021d4e5ce35
Ediney Braga <contatoedineybraga@gmail.com>
2022-10-16 16:26:15 -0300

    adding route edit link | styling with Bootstrap

--- controllers/linkController.js
+++ controllers/linkController.js
@@ -1,34 +1,37 @@
 const Link = require("../models/Link");
 
-const redirect = async (req, res) => {
+const redirect = async (req, res, next) => {
     let title = req.params.title;
 
     try{
-        let doc = await Link.findOne({title:title});
+        let doc = await Link.findOneAndUpdate({title:title}, {$inc: {click: 1}});
         console.log(doc);
-        res.redirect(doc.url);
+        if(doc) {
+            res.redirect(doc.url);
+        } else {
+            next();
+        }
     } catch (error) {
         res.send(error);
     }
 }
 
 const addLink = async (req, res) => {
-    let link = Link(req.body);
+    let link = new Link(req.body);
 
     try{
         let doc = await link.save();
-        res.send("Link adcionado com sucesso");
+        res.redirect("/");
     } catch (error){
-        res.render("index", {error, body:req.body });
+        res.render("add", {error, body:req.body });
     }
 }
 
 const allLinks = async (req, res) => {
     try{
-        let links = await Link.find({});
-        res.render("all", {links});
+        let docs = await Link.find({});
+        res.render("./all", {links:docs});
     } catch (error) {
-        // res.render(error, {body:req.body});
         res.send(error);
     }
 }
@@ -40,12 +43,44 @@ const deleteLink = async (req, res) =>{
     }
 
     try{
-        res.send(await Link.findByIdAndDelete(id))
+        await Link.findByIdAndDelete(id)
+        res.redirect("/")
     } catch (error) {
-        res.send(error);
+        res.status(404).send(error);
     }
 
 }
 
+const loadLink = async (req, res) =>{
+    let id = req.params.id;
 
-module.exports = {redirect, addLink, allLinks, deleteLink};
+    try{
+       let doc = await Link.findById(id)
+        res.render("edit", {error:false, body: doc});
+    } catch (error) {
+        res.status(404).send(error);
+    }
+}
+
+const editLink = async (req, res) => {
+    let link = {};
+    link.title = req.body.title;
+    link.description = req.body.description;
+    link.url = req.body.url;
+
+    let id = req.params.id;
+    if(!id){
+        id = req.body.id;
+    }
+
+    try{
+        let doc = await Link.updateOne({_id:id}, link);
+        res.redirect("/");
+    } catch (error){
+        res.render("edit", {error, body:req.body });
+    }
+}
+
+
+
+module.exports = {redirect, addLink, allLinks, deleteLink, loadLink, editLink};
--- routes/linkRoute.js
+++ routes/linkRoute.js
@@ -1,18 +1,21 @@
 const express = require("express");
 const router = express.Router();
 const linkController = require("../controllers/linkController");
+var methodOverride = require('method-override');
 
+router.use(methodOverride('_method'));
+router.get("/", linkController.allLinks)
+router.get("/add", (req, res) => { res.render("add.ejs", {error:false, body:{}})});
+router.get("/bancodb/:title", linkController.redirect);
+router.get("/edit/:id", linkController.loadLink);
 
-router.get("/", (req, res) => { res.render("index.ejs", {error:false, body:{}})});
-
-router.get("/all", linkController.allLinks)
-
-router.get("/bancodb/:title", linkController.redirect)
 
 router.post("/", express.urlencoded({extended:true}), linkController.addLink);
+router.post("/edit/:id", express.urlencoded({extended:true}), linkController.editLink);
+
 
 router.delete("/:id", linkController.deleteLink);
-router.delete("/", express.urlencoded({extended:true}), linkController.deleteLink);
+router.delete("/", express.json(), linkController.deleteLink);
 
 
 module.exports = router;
